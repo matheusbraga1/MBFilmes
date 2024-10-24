0a6b95056564fd2297ceae58389b9663a15d6842
Matheus Braga <matheusgalaxytab@gmail.com>
2024-10-24 15:55:43 -0400

    WIP - Initial version

---
+++ src/MBFilmes.Core/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿namespace MBFilmes.Core.Models;
+
+public class Movie
+{
+    public long Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Overview { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+    public bool IsFavorite { get; set; } = false;
+    public bool IsWatched { get; set; } = false;
+
+    public long ProfileId { get; set; }
+    public Profile Profile { get; set; } = null!;
+}
---
+++ src/MBFilmes.Core/Models/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace MBFilmes.Core.Models;
+
+public class Profile
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+    public string UserId { get; set; } = string.Empty;
+
+    public ICollection<Movie> Movies { get; set; } = [];
+}
