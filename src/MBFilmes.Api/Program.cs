5c32bc9fe3c5bc607876838612c3b92e01d5fb5c
Matheus Braga <matheusgalaxytab@gmail.com>
2024-10-29 16:27:25 -0400

    feat: registering db context

--- src/MBFilmes.Api/Program.cs
+++ src/MBFilmes.Api/Program.cs
@@ -1,4 +1,14 @@
+using MBFilmes.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+     x.UseSqlServer(connectionString);
+});
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(x =>
