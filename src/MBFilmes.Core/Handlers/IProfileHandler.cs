360281b5f7de24e3c2734b479791f4329a9427fa
Matheus Braga <matheusgalaxytab@gmail.com>
2024-10-29 16:22:39 -0400

    feat: adding handler

---
+++ src/MBFilmes.Core/Handlers/IProfileHandler.cs
@@ -0,0 +1,14 @@
+using MBFilmes.Core.Models;
+using MBFilmes.Core.Requests.Profiles;
+using MBFilmes.Core.Responses;
+
+namespace MBFilmes.Core.Handlers;
+
+public interface IProfileHandler
+{
+    Task<BaseResponse<Profile>> CreateAsync(CreateProfileRequest request);
+    Task<BaseResponse<Profile>> UpdateAsync(UpdateProfileRequest request);
+    Task<BaseResponse<Profile>> DeleteAsync(DeleteProfileRequest request);
+    Task<BaseResponse<Profile>> GetByIdAsync(GetProfileByIdRequest request);
+    Task<BaseResponse<List<Profile>>> GetAllAsync(GetAllProfilesRequest request);
+}
