6bb86ff7a42084d8c55c26e7c46066a58d3654a3
Matheus Braga <matheusgalaxytab@gmail.com>
2024-11-05 16:40:20 -0400

    feat: adding id property

--- src/MBFilmes.Core/Requests/Profiles/UpdateProfileRequest.cs
+++ src/MBFilmes.Core/Requests/Profiles/UpdateProfileRequest.cs
@@ -4,6 +4,8 @@ namespace MBFilmes.Core.Requests.Profiles;
 
 public class UpdateProfileRequest : BaseRequest
 {
+    public long Id { get; set; }
+    
     [Required(ErrorMessage = "Nome inválido")]
     [MaxLength(40, ErrorMessage = "O nome deve conter até 40 caracteres")]
     public string Name { get; set; } = string.Empty;
