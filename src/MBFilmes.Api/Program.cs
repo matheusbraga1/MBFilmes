5d7a11395fe08b7bed32554199d087062615edd8
Matheus Braga <matheusgalaxytab@gmail.com>
2024-11-05 16:39:59 -0400

    feat: adding registering profile handler as a service and adding health check endpoint

--- src/MBFilmes.Api/Program.cs
+++ src/MBFilmes.Api/Program.cs
@@ -1,26 +1,28 @@
 using MBFilmes.Api.Data;
+using MBFilmes.Api.Endpoints;
+using MBFilmes.Api.Handlers;
+using MBFilmes.Core.Handlers;
+using MBFilmes.Core.Models;
+using MBFilmes.Core.Requests.Profiles;
+using MBFilmes.Core.Responses;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? string.Empty;
 
-builder.Services.AddDbContext<AppDbContext>(x =>
-{
-     x.UseSqlServer(connectionString);
-});
+builder.Services.AddDbContext<AppDbContext>(x => { x.UseSqlServer(connectionString); });
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(x =>
-{
-     x.CustomSchemaIds(n => n.FullName);
-});
+builder.Services.AddSwaggerGen(x => { x.CustomSchemaIds(n => n.FullName); });
+builder.Services.AddTransient<IProfileHandler, ProfileHandler>();
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => new { message = "OK" });
+app.MapEndpoints();
 
 app.Run();
